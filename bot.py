a47ca5de84d08d470f0997babc5113ae13a71df1
gbtami <gbtami@gmail.com>
2022-02-07 00:56:16 +0100

    More role based mention

--- bot.py
+++ bot.py
@@ -19,7 +19,36 @@ GAME_SEEK_CHANNEL_ID = 823862902648995910
 TOURNAMENT_CHANNEL_ID = 861234739820888074
 ANNOUNCEMENT_CHANNEL_ID = 865964574507008000
 
-TOURNAMENT_GLADIATOR_ID = 867894147900637215
+ROLES = {
+    "gladiator": 867894147900637215,
+    "crazyhouse": 658544490830757919,
+    "capablanca": 658544637467951124,
+    "grand": 658544867269541899,
+    "atomic": 867889563087274034,
+    "hoppelpoppel": 867889843357876254,
+    "xiangqi": 658544904011644938,
+    "shogi": 658544950677340161,
+    "shogun": 675143932912599041,
+    "seirawan": 658753848982110209,
+    "shako": 658544983623860235,
+    "janggi": 695975424433455145,
+    "makruk": 658545040234119178,
+    "sittuyin": 658545093011046420,
+    "orda": 702977517018939444,
+    "synochess": 730903272080277524,
+    "shinobi": 867889352704131132,
+    "empire": 867892839493009478,
+    "chess": 658545185571209221,
+}
+
+CATEGORIES = {
+    "chess": ("chess", "chess960", "crazyhouse", "crazyhouse960", "placement", "atomic", "atomic960"),
+    "fairy": ("capablanca", "capablanca960", "capahouse", "capahouse960", "seirawan", "seirawan960", "shouse", "grand", "grandhouse", "shako", "shogun", "hoppelpoppel"),
+    "army": ("orda", "synochess", "shinobi", "empire", "ordamirror", "chak", "chennis"),
+    "makruk": ("makruk", "makpong", "cambodian", "sittuyin", "asean"),
+    "shogi": ("shogi", "minishogi", "kyotoshogi", "dobutsu", "gorogoroplus", "torishogi"),
+    "xiangqi": ("xiangqi", "manchu", "janggi", "minixiangqi"),
+}
 
 intents = discord.Intents(messages=True, guilds=True)
 
@@ -42,6 +71,67 @@ class MyBot(Bot):
 bot = MyBot(command_prefix="!", intents=intents)
 
 
+def get_role_mentions(bot, message):
+    guild = bot.get_guild(SERVER_ID)
+    gladiator_role = guild.get_role(ROLES["gladiator"])
+    log.debug("guild, role, intent are: %s %s %s", guild, gladiator_role, gladiator_role.mention)
+
+    variant = message.split()[1]
+
+    if variant in CATEGORIES["shogi"]:
+        role = guild.get_role(ROLES["shogi"])
+
+    elif variant in CATEGORIES["makruk"]:
+        role = guild.get_role(ROLES["makruk"])
+
+    elif variant == "janggi":
+        role = guild.get_role(ROLES["janggi"])
+
+    elif variant in CATEGORIES["xiangqi"]:
+        role = guild.get_role(ROLES["xiangqi"])
+
+    elif variant == "grand":
+        role = guild.get_role(ROLES["grand"])
+
+    elif variant == "shako":
+        role = guild.get_role(ROLES["shako"])
+
+    elif variant.startswith("atomic"):
+        role = guild.get_role(ROLES["atomic"])
+
+    elif variant.startswith("crazyhouse") or variant in ("shouse", "capahouse", "capahouse960", "grandhouse"):
+        role = guild.get_role(ROLES["crazyhouse"])
+
+    elif variant.startswith("capablanca"):
+        role = guild.get_role(ROLES["capablanca"])
+
+    elif variant.startswith("seirawan"):
+        role = guild.get_role(ROLES["seirawan"])
+
+    elif variant == "hoppelpoppel":
+        role = guild.get_role(ROLES["hoppelpoppel"])
+
+    elif variant == "shogun":
+        role = guild.get_role(ROLES["shogun"])
+    elif variant.startswith("orda"):
+        role = guild.get_role(ROLES["orda"])
+    elif variant == "synochess":
+        role = guild.get_role(ROLES["synochess"])
+    elif variant == "shinobi":
+        role = guild.get_role(ROLES["shinobi"])
+    elif variant == "empire":
+        role = guild.get_role(ROLES["empire"])
+# TODO: self-roles?
+#    elif variant == "chak":
+#        role = guild.get_role(ROLES["chak"])
+#    elif variant == "chennis":
+#        role = guild.get_role(ROLES["chennis"])
+    else:
+        role = guild.get_role(ROLES["chess"])
+
+    return "%s %s" % (gladiator_role.mention, role.mention)
+
+
 async def lobby_task(bot):
     await bot.wait_until_ready()
 
@@ -57,10 +147,6 @@ async def lobby_task(bot):
 
     announcement_channel = bot.get_channel(ANNOUNCEMENT_CHANNEL_ID)
     log.debug("announcement_channel is: %s", announcement_channel)
-
-    guild = bot.get_guild(SERVER_ID)
-    role = guild.get_role(TOURNAMENT_GLADIATOR_ID)
-    log.debug("guild, role, intent are: %s %s %s", guild, role, role.mention)
 
     while True:
         log.debug("+++ Creating new aiohttp.ClientSession()")
@@ -92,7 +178,7 @@ async def lobby_task(bot):
                                 await tournament_channel.send("%s" % data['message'])
                             elif data['type'] == 'notify_tournament':
                                 log.debug("+++ notify_tournament msg: %s", data["message"])
-                                await announcement_channel.send("%s %s" % (role.mention, data['message']))
+                                await announcement_channel.send("%s %s" % (get_role_mentions(bot, data['message']), data['message']))
                     except Exception:
                         logging.exception("baj van")
                 elif msg.type == aiohttp.WSMsgType.CLOSE:
